f681faf69f1f8b5ef79d51a3410a2bf6192e3eff
t916347 <ronald.bao@telus.com>
2019-04-05 14:34:11 -0700

    make some change

--- src/main/java/com/netcracker/solutions/customers/telus/ed/integration/api/service/OrderService.java
+++ src/main/java/com/netcracker/solutions/customers/telus/ed/integration/api/service/OrderService.java
@@ -4,6 +4,6 @@ package com.netcracker.solutions.customers.telus.ed.integration.api.service;
  * Created by Ronald Bao on 18/03/2019.
  */
 public interface OrderService {
-    String placeOrder(String fName, String lName, String refNumber);
-    boolean cancelOrder(String refNumber);
+    String placeOrder(String fName, String lName,  String refNumber);
+    boolean cancelOrder(String  refNumber);
 }
